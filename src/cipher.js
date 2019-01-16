3d5f227beb5bf226028c5b2a9e17097536488b2d
Cristalgarcia <solocrissgar@gmail.com>
2019-01-16 13:05:10 -0600

    Modificación de html, js para llamar

--- src/cipher.js
+++ src/cipher.js
@@ -6,9 +6,18 @@ window.cipher = {
       let codigo=(string.charCodeAt(i) - 65 + offset) % 26 + 65;
       let resultado=String.fromCharCode(codigo);
       textCifrado+= resultado;
-    }
-    document.getElementById('decifrar').innerHTML = textCifrado;
-    /*console.log(textCifrado)*/ 
+    } 
     return textCifrado
-  }
+  },
+
+  decode:(offset, string) =>{
+    let textDecifrado= "";
+    //Recorrer cada uno de los caracteres del mensaje
+    for (let i = 0; i < string.length; i++){
+       let codigo=(string.charCodeAt(i) + 65 - offset) % 26 + 65;
+      let resultado=String.fromCharCode(codigo);
+      textDecifrado+= resultado;
+    }
+    return textDecifrado
+  }  
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-const getCifrado = () =>{ 
+const getCifrar = () =>{ 
 const offset = parseInt(document.getElementById('clave').value);
 //console.log(espacio)
 const string = document.getElementById('cifrar').value.toUpperCase();
@@ -7,17 +7,10 @@ const string = document.getElementById('cifrar').value.toUpperCase();
 document.getElementById('decifrar').value = cipher.encode(offset, string);
 }
 
-
-
-/*const getCifrado (respuesta)=> {
-    const offset = parseInt(documentById('clave').value);
-    const string = document.getElementById('cifrar').value;
-    document.getElementById('cifrar').value = '';
-    document.getElementById('decifrar').value = cipher.encode(offset, string);
-}*/
-
-/*let offset = parseInt(document.getElementById('clave').value);
-let string = document.getElementById('cifrar').value;
-const enviar = document.getElementById("mensajeCifrado");
-enviar.addEventListener('click',window.cipher.encode);
-document.getElementById('decifrar').innerHTML = texCifrado;*/
+const getDecifrar = () =>{ 
+const offset = parseInt(document.getElementById('clave').value);
+//console.log(espacio)
+const string = document.getElementById('cifrar').value.toUpperCase();
+//console.log
+document.getElementById('decifrar').value = cipher.decode(offset, string);
+}
